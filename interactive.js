19115ba8d649354f00b276a291ffc805f394e6fc
DJ475 <149053710+DJ475@users.noreply.github.com>
2023-11-28 19:34:00 -0700

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,4 +1,37 @@
 function func1()
 {
-    
+    alert
+    (
+        " The summary of this page: 'In this blog i will be mentioning why and how experts become sucessful as well as the best practices into becoming a good expert'"
+    );
+}
+
+function func2()
+{
+    alert
+    (
+        " The summary of this page: ''"
+    );
+}
+
+function func3()
+{
+    alert
+    (
+        " The summary of this page: ''"
+    );
+}
+
+function size_works_cited()
+{
+    var size_change = document.getElementById("credits_text");
+
+    size_change.style.width = "auto";
+
+    size_change.style.height = "auto";
+}
+
+window.onload()
+{
+    size_works_cited();
 }
