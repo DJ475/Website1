24c69a8eea3d078fcc80947919e3c354abee2624
DJ475 <149053710+DJ475@users.noreply.github.com>
2023-11-29 03:30:31 -0700

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -14,7 +14,7 @@ function func2()
         " The summary of this page:" + "\n" +
         "* Being open minded allows for a more extensive learning process." + "\n" + 
         "* Professionals that fail usually end up being more close minded when wrong which stifles thier knowledge" + "\n" + 
-        "* When openmindedness is achieved then a person is able to be a reliable expert with opinions about  "
+        "* When openmindedness is achieved then a person is able to be a reliable expert with opinions about."
     );
 }
 
@@ -25,7 +25,7 @@ function func3()
         " The summary of this page:" + "\n" +
         "*Flaws are part of human nature allowing one to grow and learn from mistakes" + "\n" +
         "*When humans are wrong they do not admit to faults and then are unversed in the right answer" + "\n" +
-        "*Their overall professional credibility is then questioned and they become less reliable"
+        "*Their overall professional credibility is then questioned and they become less reliable."
     );
 
 
