b7a94759e5273e3c957f912105f73e2520fd2f04
DJ475 <149053710+DJ475@users.noreply.github.com>
2023-11-30 20:46:28 -0700

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,35 +1,17 @@
-function func1()
-{
-    alert
-    (
-        " The summary of this page:" + "\n" + 
-        "In this blog i will be mentioning why and how experts become sucessful as well as the best practices into becoming a good expert."
-    );
-}
+
 
 function func2()
 {
-    alert
-    (
-        " The summary of this page:" + "\n" +
-        "* Being open minded allows for a more extensive learning process." + "\n" + 
-        "* Professionals that fail usually end up being more close minded when wrong which stifles thier knowledge" + "\n" + 
-        "* When openmindedness is achieved then a person is able to be a reliable expert with opinions about."
-    );
+    alert("When a person is able to improve overall flaws with human thier overall expertise improves as well as professionalism, due to be being knowledgable in 2 different sides");
 }
 
-function func3()
-{
-    alert
-    (
-        " The summary of this page:" + "\n" +
-        "*Flaws are part of human nature allowing one to grow and learn from mistakes" + "\n" +
-        "*When humans are wrong they do not admit to faults and then are unversed in the right answer" + "\n" +
-        "*Their overall professional credibility is then questioned and they become less reliable."
-    );
-
-
-}
+// function func3()
+// {
+//     alert
+//     (
+//         " The summary of this page:" + "\n" +
+//     );
+// }
 
 function size_works_cited()
 {
