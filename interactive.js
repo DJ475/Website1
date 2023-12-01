dff39155f4e553cfb73a39b3c41918ed0196ce1c
DJ475 <149053710+DJ475@users.noreply.github.com>
2023-11-30 21:12:30 -0700

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -5,22 +5,11 @@ function func2()
     alert("When a person is able to improve overall flaws with human thier overall expertise improves as well as professionalism, due to be being knowledgable in 2 different sides");
 }
 
-// function func3()
-// {
-//     alert
-//     (
-//         " The summary of this page:" + "\n" +
-//     );
-// }
-
-function size_works_cited()
+function func3()
 {
-    var size_change = document.getElementById("credits_text");
-
-    size_change.style.width = "auto";
-
-    size_change.style.height = "auto";
+    alert("Human nature is a major impact on how we behave, one such being that humans make mistakes. To look at these mistakes gives one the ability to grow and prosper in their feild");
 }
+
 
 window.onload()
 {
